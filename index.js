2605ecc270802e9feead7395174fa6d93474f6a9
Dan Brett <boristhellama99@gmail.com>
2018-01-12 18:50:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function nowServing(array){
    if (array.length === 0) {
      return "The line is currently empty.";
    }
-   for (var i = 0; )
-   var str = ""
+   for (var i = array.length; i > 0; i--)
+   var str = "The line is currently:";
+   
  }
