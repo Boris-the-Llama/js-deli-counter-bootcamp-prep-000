3ff4560aab1ae3be9a2dc23eb158a9911063d31d
Dan Brett <boristhellama99@gmail.com>
2018-01-12 18:15:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function takeANumber(array, name) {
 
 function nowServing(array) {
     for (var i = array.length; i > 0; i--) {
-        array.shift();
-
+    array.shift();
     return "Currently serving " + array[0] + ".";
   } if (array.length === 0) {
     return 'There is nobody waiting to be served!';
