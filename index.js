65fb51781b031f916c8b828de14deaa11096c90d
Dan Brett <boristhellama99@gmail.com>
2018-01-12 17:46:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var katzDeliLine = [];
+
+function takeANumber(array, name) {
+  array.push(name);
+  var position = parseInt(array.indexOf(name) + 1);
+  return `Welcome, ${name}. You are number ` + position + ' in line.';
+}
+
+function nowServing(array) {
+  if (array.length > 0) {
+    console.log("Currently serving " + array[0] + ".");
+    array.shift();
+  } else {
+  return "There is nobody waiting to be served!";
+   }
+}
